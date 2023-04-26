ea157a6cdea1e708393b86d727efa420f11b1c2f
GrassyAirplane <euanlimcs@gmail.com>
2023-04-26 15:32:47 +0800

    Changes to Dependencies

--- server/src/test/java/com/team8/backend/ninemanmorris/GameControllerTests.java
+++ server/src/test/java/com/team8/backend/ninemanmorris/GameControllerTests.java
@@ -1,16 +1,28 @@
 package com.team8.backend.ninemanmorris;
 
-import static org.mockito.ArgumentMatchers.contains;
-
-import org.assertj.core.api.Assert;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class GameControllerTests {
 
+    private static GameController controller;
+
+    // Setup state before any tests run
+    @BeforeAll
+    public static void init() {
+        controller = new GameController();
+    }
+
+    // Cleanup state after all tests run
+    @AfterAll
+    public static void cleanup() {
+        controller = null;
+    }
+
     @Test
     public void testGameController() {
-        GameController controller = new GameController();
         Assertions.assertThat(controller.getId()).isEqualTo(10);
     }
 
