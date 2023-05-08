32a49033fdabc5f7f00620904792b8191a1b1601
GrassyAirplane <euanlimcs@gmail.com>
2023-05-08 17:38:33 +0800

    Refactoring of the Entire Board system, position system and movement system

--- server/src/main/java/com/team8/backend/ninemanmorris/Game.java
+++ server/src/main/java/com/team8/backend/ninemanmorris/Game.java
@@ -10,7 +10,17 @@ public class Game {
     private Player currPlayer;
 
     public Game() {
-        this.board = new Board();
+        int[][] initialBoard = {
+                { 1, 0, 0, 2, 0, 0, 3 },
+                { 0, 2, 0, 1, 0, 1, 0 },
+                { 0, 0, 1, 1, 1, 0, 0 },
+                { 1, 1, 1, 0, 1, 3, 1 },
+                { 0, 0, 3, 1, 1, 0, 0 },
+                { 0, 2, 0, 3, 0, 2, 0 },
+                { 2, 0, 0, 1, 0, 0, 1 }
+        };
+
+        this.board = new Board(initialBoard);
         this.playerOne = new Player("Player 1", 2);
         this.playerTwo = new Player("Player 2", 3);
         this.currPlayer = playerOne;
@@ -33,57 +43,66 @@ public class Game {
     }
 
     private boolean validateMove(Board newBoard) {
-        boolean isValid = false;
-        ArrayList<ArrayList<Integer>> currBoardState = this.board.getBoardState();
-        ArrayList<ArrayList<Integer>> newBoardState = newBoard.getBoardState();
-        for (int i = 0; i < currBoardState.size(); i++) {
-            if (currBoardState != newBoardState) {
-                // Check current row if there is difference
-                for (int j = 0; j < currBoardState.get(i).size(); j++) {
-                    if (currBoardState.get(i).get(j) != newBoardState.get(i).get(j)) {
-                        // Validate the change if spot is not unavailable, and move is made by current
-                        // player
-                        if ((currBoardState.get(i).get(j) != 1)
-                                && (newBoardState.get(i).get(j) == this.playerOne.getPlayerToken()
-                                        || newBoardState.get(i).get(j) == this.playerTwo.getPlayerToken())) {
-                            System.out.println("REACHED FALSE");
-                            return false;
-                        }
-                        if (currBoardState.get(i).get(j) == 1
-                                && newBoardState.get(i).get(j) == this.currPlayer.getPlayerToken()) {
-                            System.out.print("RECAHCES");
-                            isValid = true;
-                        } else if (currBoardState.get(i).get(j) == this.currPlayer.getPlayerToken()
-                                && newBoardState.get(i).get(j) == 1) {
-                            System.out.println("RECAHES SECOND");
-                            System.out.println(currBoardState.get(i).get(j));
-                            System.out.println(newBoardState.get(i).get(j));
-                            System.out.println(this.playerOne.getPlayerToken());
-                            System.out.println(this.playerTwo.getPlayerToken());
-                            isValid = true;
-                        }
-                    }
-                }
-            }
-        }
+        boolean isValid = true;
+        // ArrayList<ArrayList<Integer>> currBoardState = this.board.getBoardState();
+        // ArrayList<ArrayList<Integer>> newBoardState = newBoard.getBoardState();
+        // for (int i = 0; i < currBoardState.size(); i++) {
+        // if (currBoardState != newBoardState) {
+        // // Check current row if there is difference
+        // for (int j = 0; j < currBoardState.get(i).size(); j++) {
+        // if (currBoardState.get(i).get(j) != newBoardState.get(i).get(j)) {
+        // // Validate the change if spot is not unavailable, and move is made by
+        // current
+        // // player
+        // if ((currBoardState.get(i).get(j) != 1)
+        // && (newBoardState.get(i).get(j) == this.playerOne.getPlayerToken()
+        // || newBoardState.get(i).get(j) == this.playerTwo.getPlayerToken())) {
+        // System.out.println("REACHED FALSE");
+        // return false;
+        // }
+        // if (currBoardState.get(i).get(j) == 1
+        // && newBoardState.get(i).get(j) == this.currPlayer.getPlayerToken()) {
+        // System.out.print("RECAHCES");
+        // isValid = true;
+        // } else if (currBoardState.get(i).get(j) == this.currPlayer.getPlayerToken()
+        // && newBoardState.get(i).get(j) == 1) {
+        // System.out.println("RECAHES SECOND");
+        // System.out.println(currBoardState.get(i).get(j));
+        // System.out.println(newBoardState.get(i).get(j));
+        // System.out.println(this.playerOne.getPlayerToken());
+        // System.out.println(this.playerTwo.getPlayerToken());
+        // isValid = true;
+        // }
+        // }
+        // }
+        // }
+        // }
         return isValid;
     }
 
-    public boolean makeMove(Board newBoard) {
-        Boolean isValid = false;
-        isValid = this.validateMove(newBoard);
-
+    public boolean makeMove(int fromRow, int fromCol, int toRow, int toCol) {
+        Boolean isValid = true;
+        // Add validation for move
         if (isValid) {
-            setBoard(newBoard);
-            // Swap player turns
-            if (this.getCurrPlayer() == this.playerOne) {
-                this.setCurrPlayer(this.playerTwo);
-            } else if (this.getCurrPlayer() == this.playerTwo) {
-                this.setCurrPlayer(this.playerOne);
-            }
+            this.board.makeMove(fromRow, fromCol, toRow, toCol);
+            // Can now add validation for after move
+            // Maybe swap players maybe not depending
+            // Maybe changing phase
+            this.swapPlayers();
         }
 
         return isValid;
+    }
+
+    /**
+     * Helper function for Swaping the Players
+     */
+    private void swapPlayers() {
+        if (this.currPlayer == playerOne) {
+            this.currPlayer = playerTwo;
+        } else {
+            this.currPlayer = playerOne;
+        }
     }
 
 }
--- server/src/main/java/com/team8/backend/ninemanmorris/Position.java
+++ server/src/main/java/com/team8/backend/ninemanmorris/Position.java
@@ -1,34 +1,7 @@
 package com.team8.backend.ninemanmorris;
 
-import java.util.ArrayList;
-import java.util.List;
+public interface Position {
+    boolean canEnter();
 
-public class Position {
-
-    private List<Position> neighbors = new ArrayList<>();
-
-    // private Board board;
-
-    private int row;
-
-    private int col;
-
-    public Position(int row, int col) {
-        this.row = row;
-        this.col = col;
-    };
-
-    public void addNeighbors(Position[] newNeighbors) {
-        for (Position neighbor : newNeighbors) {
-            this.neighbors.add(neighbor);
-        }
-    };
-
-    public int getRow() {
-        return this.row;
-    };
-
-    public int getCol() {
-        return this.col;
-    };
+    Token getToken();
 }
