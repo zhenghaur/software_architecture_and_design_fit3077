4ea6cf736fa9b627094db7adf5afa90f782ee7d4
GrassyAirplane <euanlimcs@gmail.com>
2023-05-13 13:09:22 +0800

    Added Phase enums, altered Player, added placement phase functionality

---
+++ server/src/main/java/com/team8/backend/ninemanmorris/Phase.java
@@ -0,0 +1,23 @@
+package com.team8.backend.ninemanmorris;
+
+/**
+ * Enum class for the different movement phases
+ * For token movement options, this will set
+ * the differnet types of checks based on input
+ */
+public enum Phase {
+
+    PLACEMENT(0),
+    MOVEMENT(1),
+    REMOVE(2);
+
+    private final int phase;
+
+    Phase(int phase) {
+        this.phase = phase;
+    }
+
+    public int getPhase() {
+        return phase;
+    }
+}
