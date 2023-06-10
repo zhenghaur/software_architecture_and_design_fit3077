2f123bd1eed228567a24d6b1c9cf2d9c08195e78
Lim Zheng Haur <zlim0038@student.monash.edu>
2023-06-10 19:31:48 +1000

    added try catch

--- server/src/main/java/com/team8/backend/ninemanmorris/game/Game.java
+++ server/src/main/java/com/team8/backend/ninemanmorris/game/Game.java
@@ -109,32 +109,36 @@ public class Game {
      * 
      */
     public boolean setState(int playerOneTokesLeft, int playerTwoTokensLeft, int playerOneTokensStorage, int playerTwoTokensStorage, boolean gameOver, int playerTurn, int playerPhase, int[][] boardState){
-        // set both players number of tokens left
-        this.playerOne.setNumTokens = playerOneTokensLeft;
-        this.playerTwo.setNumTokens = playerTwoTokensLeft;
+        try {
+            // set both players number of tokens left
+            this.playerOne.setNumTokens = playerOneTokensLeft;
+            this.playerTwo.setNumTokens = playerTwoTokensLeft;
 
-        // set both players number of tokens in storage
-        this.playerOne.setNumStorageTokens = playerOneTokensStorage;
-        this.playerTwo.setNumStorageTokens = playerTwoTokensStorage;
+            // set both players number of tokens in storage
+            this.playerOne.setNumStorageTokens = playerOneTokensStorage;
+            this.playerTwo.setNumStorageTokens = playerTwoTokensStorage;
 
-        // set game state
-        this.gameStatus = gameOver;
+            // set game state
+            this.gameStatus = gameOver;
 
-        // set player turn
-        this.currPlayer = (playerTurn == 1) ? this.playerOne : this.playerTwo;
+            // set player turn
+            this.currPlayer = (playerTurn == 1) ? this.playerOne : this.playerTwo;
 
-        // set player phase
-        if (playerPhase == 0){
-            this.currPlayer.setMovementPhase(Phase.PLACEMENT);
-        } else if (playerPhase == 1){
-            this.currPlayer.setMovementPhase(Phase.MOVEMENT);
-        } else {
-            this.currPlayer.setMovementPhase(Phase.REMOVE);
+            // set player phase
+            if (playerPhase == 0){
+                this.currPlayer.setMovementPhase(Phase.PLACEMENT);
+            } else if (playerPhase == 1){
+                this.currPlayer.setMovementPhase(Phase.MOVEMENT);
+            } else {
+                this.currPlayer.setMovementPhase(Phase.REMOVE);
+            }
+
+            // set board state
+            this.board = new Board(boardState);
+        } catch (Exception e) {
+            return false
         }
-
-        // set board state
-        this.board = new Board(boardState);
-
+        
         return true;
     }
 
